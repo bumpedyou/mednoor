ca5602348e6f22bcf2d2c597e9954d022c540281
Heriberto J <heribertojuarezj@gmail.com>
2021-11-07 18:04:55 -0600

    Improve compilation speed

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,6 +109,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    hardSource: true,
+    cache: true,
+    parallel: true,
     loaders: {
       sass: {
         implementation: require('sass'),
